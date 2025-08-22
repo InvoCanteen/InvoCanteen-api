1c6f54ba7d6eea6d9299585b1eb505d0545812a4
Adil Aulia Azuri <adil.azuri145@gmail.com>
2025-08-22 14:03:48 +0700

    [FEAT]-BE-Day-2 : add endpoint get all product

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -42,3 +42,34 @@ export async function add_Product(req: Request, res: Response) {
         res.json({ message: error });
     }
 }
+
+export async function get_All_Product(req: Request, res: Response) {
+    const { sortBy, order, minPrice, maxPrice, limit, offset } = req.query
+
+    const filters: any = {}
+    if (minPrice) filters.price = { gte: parseFloat(minPrice as string) }
+    if (maxPrice) {
+        filters.price = {
+            ...(filters.price || {}),
+            lte: parseFloat(maxPrice as string)
+        }
+    }
+    try {
+        const products = await prisma.product.findMany(
+            {
+                where: {
+                    ...filters,
+                },
+                orderBy: {
+                    [sortBy as string]: order as "asc" | "desc"
+                },
+                take: Number(limit),
+                skip: Number(offset)
+            }
+        )
+        const total = await prisma.product.count({ where: filters })
+        res.status(200).json({ data: products, total })
+    } catch (error) {
+        res.json({ message: error });
+    }
+}
--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { add_Product } from "../controllers/productController";
+import { add_Product, get_All_Product } from "../controllers/productController";
 import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router = Router();
 
 router.post("/product/add", authMiddleware, add_Product);
+router.get("/product/get", authMiddleware, get_All_Product);
 
 
 export default router;
