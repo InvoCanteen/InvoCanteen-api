1fc6ae6d302223863fc4463358f4992219fbf29a
Adil Aulia Azuri <adil.azuri145@gmail.com>
2025-08-24 20:35:50 +0700

    [FEAT]-BE-Day-2 : add feature logout, profile user, update password, and features update product, detail product and delete product

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -103,3 +103,138 @@ export async function getAllProducts(req: Request, res: Response) {
         });
     }
 }
+
+export async function updateProduct(req: Request, res: Response) {
+    const userRole = (req as any).user.role;
+    const { id } = req.params;
+
+    if (!userRole) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+    if (userRole !== "ADMIN") {
+        return res.status(403).json({ message: "Only admin can update products" });
+    }
+    if (!id) {
+        return res.status(400).json({ message: "Product ID is missing." });
+    }
+
+    const productId = parseInt(id);
+    if (isNaN(productId)) {
+        return res.status(400).json({ message: "Invalid product ID" });
+    }
+
+    try {
+        const { name, price, categoryId, description } = req.body;
+        const file = req.file;
+        const dataToUpdate: any = {};
+        if (name !== undefined) dataToUpdate.name = name;
+        if (price !== undefined) dataToUpdate.price = price;
+        if (categoryId !== undefined) dataToUpdate.categoryId = parseInt(categoryId) || null;
+        if (description !== undefined) dataToUpdate.description = description;
+
+        if (file) {
+            try {
+                const uploadResult = await new SupabaseUploadService().uploadFile(file);
+
+                dataToUpdate.imageProduct = uploadResult.url;
+            } catch (uploadError) {
+                console.error("Supabase upload error:", uploadError);
+                return res.status(400).json({
+                    code: 400,
+                    status: "error",
+                    message: "File upload failed",
+                    cause: uploadError instanceof Error ? uploadError.message : "Unknown upload error"
+                });
+            }
+        }
+
+        const existingProduct = await prisma.product.findUnique({ where: { id: productId } });
+        if (!existingProduct) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        const updatedProduct = await prisma.product.update({
+            where: { id: productId },
+            data: dataToUpdate,
+        });
+
+        res.status(200).json({
+            message: "Product updated successfully",
+            product: updatedProduct,
+        });
+    } catch (error) {
+        console.error("Error updating product:", error);
+        res.status(500).json({
+            message: "An internal server error occurred."
+        });
+    }
+}
+
+export async function detailProduct(req: Request, res: Response) {
+    const { id } = req.params;
+
+    if (!id) {
+        return res.status(404).json({
+            message: "Product Invalid",
+        });
+    }
+
+    try {
+        const product = await prisma.product.findUnique({
+            where: {
+                id: parseInt(id),
+            },
+        });
+
+        if (!product) {
+            return res.status(404).json({
+                message: "Product not found",
+            });
+        }
+
+        res.status(200).json({
+            data: product,
+        });
+    } catch (error) {
+        console.error("Error fetching product detail:", error);
+        res.status(500).json({
+            message: "An internal server error occurred.",
+        });
+    }
+}
+
+export async function deleteProduct(req: Request, res: Response) {
+    const { id } = req.params;
+
+    if (!id) {
+        return res.status(400).json({
+            message: "Invalid product ID",
+        });
+    }
+
+    try {
+        const product = await prisma.product.findUnique({
+            where: { id: parseInt(id) }
+        });
+
+        if (!product) {
+            return res.status(404).json({
+                message: "Product not found",
+            });
+        }
+
+        await prisma.product.delete({
+            where: { id: parseInt(id, 10) },
+        });
+
+        res.status(200).json({
+            message: "Product deleted successfully",
+            data: product,
+        });
+    } catch (error) {
+        console.error("Error deleting product:", error);
+        res.status(500).json({
+            message: "An internal server error occurred.",
+        });
+    }
+}
