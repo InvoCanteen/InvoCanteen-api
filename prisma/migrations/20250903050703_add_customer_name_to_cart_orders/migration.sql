aaa56260b15ac33d003aa74addafb7bd746fef48
Adil Aulia Azuri <adil.azuri145@gmail.com>
2025-09-08 09:46:29 +0700

    fix : on delete cart and cart item

--- prisma/migrations/20250903050703_add_customer_name_to_cart_orders/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Cart" ADD COLUMN     "customerName" TEXT;
-
--- AlterTable
-ALTER TABLE "public"."orders" ADD COLUMN     "customerName" TEXT;
---
+++ prisma/migrations/20250908021919_init/migration.sql
@@ -0,0 +1,151 @@
+-- CreateEnum
+CREATE TYPE "public"."CartStatus" AS ENUM ('ACTIVE', 'ABANDONED', 'CHECKED_OUT');
+
+-- CreateEnum
+CREATE TYPE "public"."PayStatus" AS ENUM ('UNPAID', 'PAID');
+
+-- CreateEnum
+CREATE TYPE "public"."UserRole" AS ENUM ('ADMIN', 'CASHIER');
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "photoProfile" TEXT,
+    "role" "public"."UserRole" NOT NULL DEFAULT 'CASHIER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "categoryId" INTEGER,
+    "price" DECIMAL(10,2) NOT NULL,
+    "description" TEXT,
+    "imageProduct" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Cart" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER,
+    "customerName" TEXT,
+    "subtotal" DECIMAL(10,2) NOT NULL DEFAULT 0,
+    "tax" DECIMAL(10,2) NOT NULL DEFAULT 0,
+    "total" DECIMAL(10,2) NOT NULL DEFAULT 0,
+    "status" "public"."CartStatus" NOT NULL DEFAULT 'ACTIVE',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Cart_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."CartItem" (
+    "id" SERIAL NOT NULL,
+    "cartId" INTEGER NOT NULL,
+    "productId" INTEGER,
+    "quantity" INTEGER NOT NULL DEFAULT 1,
+    "price" DECIMAL(10,2) NOT NULL,
+    "subtotal" DECIMAL(10,2) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "CartItem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."orders" (
+    "id" SERIAL NOT NULL,
+    "sourceCartId" INTEGER,
+    "userId" INTEGER,
+    "customerName" TEXT,
+    "subtotal" DECIMAL(10,2) NOT NULL DEFAULT 0,
+    "tax" DECIMAL(10,2) NOT NULL DEFAULT 0,
+    "total" DECIMAL(10,2) NOT NULL DEFAULT 0,
+    "payStatus" "public"."PayStatus" NOT NULL DEFAULT 'UNPAID',
+    "paidAt" TIMESTAMP(3),
+    "notes" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "orders_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."OrderItem" (
+    "id" SERIAL NOT NULL,
+    "orderId" INTEGER NOT NULL,
+    "productId" INTEGER,
+    "quantity" INTEGER NOT NULL DEFAULT 1,
+    "price" DECIMAL(10,2) NOT NULL,
+    "subtotal" DECIMAL(10,2) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "OrderItem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "public"."Category"("name");
+
+-- CreateIndex
+CREATE INDEX "Product_categoryId_idx" ON "public"."Product"("categoryId");
+
+-- CreateIndex
+CREATE INDEX "Cart_userId_idx" ON "public"."Cart"("userId");
+
+-- CreateIndex
+CREATE INDEX "Cart_status_idx" ON "public"."Cart"("status");
+
+-- CreateIndex
+CREATE INDEX "orders_userId_idx" ON "public"."orders"("userId");
+
+-- CreateIndex
+CREATE INDEX "orders_payStatus_idx" ON "public"."orders"("payStatus");
+
+-- CreateIndex
+CREATE INDEX "orders_createdAt_idx" ON "public"."orders"("createdAt");
+
+-- AddForeignKey
+ALTER TABLE "public"."Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "public"."Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Cart" ADD CONSTRAINT "Cart_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."CartItem" ADD CONSTRAINT "CartItem_cartId_fkey" FOREIGN KEY ("cartId") REFERENCES "public"."Cart"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."CartItem" ADD CONSTRAINT "CartItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "public"."Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."orders" ADD CONSTRAINT "orders_sourceCartId_fkey" FOREIGN KEY ("sourceCartId") REFERENCES "public"."Cart"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."orders" ADD CONSTRAINT "orders_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."OrderItem" ADD CONSTRAINT "OrderItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "public"."orders"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."OrderItem" ADD CONSTRAINT "OrderItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "public"."Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
